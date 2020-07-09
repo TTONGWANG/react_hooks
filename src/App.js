8f7b8efeb889dfea9bf0c21a40109f2b6c9e4ad0
wangtapplication@outlook.com <wangtapplication@outlook.com>
2020-07-09 15:02:35 +0800

    101 build class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+      }
+  }
+
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+   
+    // this.setState((state) =>({
+    //   count: state.count + 1
+    // }))
+    console.log(this.state.count)
+  }
+
+  render() { 
+    return (  
+      <div>
+        <p>click{this.state.count}times</p>
+        <button onClick = {this.handleClick}>click</button>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
